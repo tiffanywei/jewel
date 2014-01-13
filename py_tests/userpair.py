e8c188974dcec041326f8ac87fb2657a59742e35
Mode Chusky <modechusky@gmail.com>
2014-01-12 16:17:13 -0800

    Added test for UserPair.get_record_logs

--- py_tests/userpair.py
+++ py_tests/userpair.py
@@ -11,3 +11,15 @@ class TestUserPair(unittest.TestCase):
     up.add('RecordLog:1', 0)
     self.assertTrue('UserPair:tiffy:vlad' in self.fake_redis.state)
     self.assertTrue('0___RecordLog:1' in self.fake_redis.state['UserPair:tiffy:vlad'])
+
+  def test_get_record_logs(self):
+    up = UserPair('vlad', 'tiffy', self.fake_redis)
+    up.add('RecordLog:3', 2)
+    up.add('RecordLog:1', 0)
+    up.add('RecordLog:2', 1)
+    record_logs = up.get_record_logs( 0, 3)
+    self.assertEquals(['RecordLog:1', 'RecordLog:2', 'RecordLog:3'], record_logs)
+    record_logs = up.get_record_logs(0)
+    self.assertEquals(['RecordLog:1', 'RecordLog:2', 'RecordLog:3'], record_logs)
+    record_logs = up.get_record_logs()
+    self.assertEquals(['RecordLog:1', 'RecordLog:2', 'RecordLog:3'], record_logs)
