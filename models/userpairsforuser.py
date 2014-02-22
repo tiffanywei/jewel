6f95e36c9372661422e49e38976bfb17d212b0b9
Mode Chusky <modechusky@gmail.com>
2014-02-22 15:29:31 -0800

    make timestamp param to UserPairsForUser#add_secondary_user optional. Reuse redis client in get_user_pairs

--- models/userpairsforuser.py
+++ models/userpairsforuser.py
@@ -1,3 +1,6 @@
+import time
+from models.userpair import UserPair
+
 class UserPairsForUser(object):
   """
   Read and modify the sorted set of secondary users associated with a given primary user.
@@ -11,14 +14,15 @@ class UserPairsForUser(object):
     self._primary_user = primary_user
     self._user_pairs_for_user_key = self._make_key()
 
-  def add_secondary_user(secondary_user, timestamp):
+  def add_secondary_user(self, secondary_user, timestamp=None):
+    timestamp = timestamp or time.time()
     self._redis.zadd(self._user_pairs_for_user_key, timestamp, secondary_user)
 
   def get_user_pairs(self):
     secondary_users = self._redis.zrange(self._user_pairs_for_user_key, 0, -1)
     user_pairs_by_secondary_user = {}
     for secondary_user in secondary_users:
-      user_pairs_by_secondary_user[secondary_user] = UserPair(self._primary_user, secondary_user)
+      user_pairs_by_secondary_user[secondary_user] = UserPair(self._primary_user, secondary_user, self._redis)
     return user_pairs_by_secondary_user
 
 
